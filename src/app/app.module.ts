de4df60f4dc0c10ed6a095a91cf9b2d2e141709f
vipin <vipin.shukla@livnsense.com>
2021-08-03 00:36:42 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ForgotPasswordComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+export class ForgotPasswordComponent implements OnInit {
+
+  constructor() { 
+   
+  }
+
+  ngOnInit(): void {
+  }
+
+}
